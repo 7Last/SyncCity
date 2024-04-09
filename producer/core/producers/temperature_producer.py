91db3d66ab770f847438848426d2164155e5d182
elenaferr0 <elena.ferro@gmail.com>
2024-04-09 18:48:03 +0200

    feat: generating temperature values with sinusoid

--- producer/core/producers/temperature_producer.py
+++ producer/core/producers/temperature_producer.py
@@ -1,9 +1,32 @@
+from math import pi, sin
 from datetime import datetime, timedelta
 import random
 import asyncio
 
 from .producer import Producer
 from ..models.temperature_raw_data import TemperatureRawData
+
+
+def _sinusoidal_value(timestamp: datetime) -> float:
+    season_coefficient = 0
+    thermal_excursion = 0
+    match timestamp.month:
+        case 1 | 2 | 12:  # winter
+            season_coefficient = 5
+            thermal_excursion = 10
+        case 3 | 4 | 5:  # spring
+            season_coefficient = 10
+            thermal_excursion = 15
+        case 6 | 7 | 8:  # summer
+            season_coefficient = 22
+            thermal_excursion = 20
+        case 9 | 10 | 11:  # autumn
+            season_coefficient = 7
+            thermal_excursion = 15
+
+    x = timestamp.hour + timestamp.minute / 60
+    noise = random.uniform(0, 0.4)
+    return thermal_excursion * sin(x * pi / 24) + season_coefficient + noise
 
 
 class TemperatureProducer(Producer):
@@ -17,11 +40,11 @@ class TemperatureProducer(Producer):
     async def stream(self) -> TemperatureRawData:
         while self.limit != 0 and self.running:
             self.limit -= 1
-            self.timestamp = self.timestamp + self.frequency
+            self.timestamp += self.frequency
             await asyncio.sleep(self.frequency.total_seconds())
 
             yield TemperatureRawData(
-                value=random.uniform(-10, 30),
+                value=_sinusoidal_value(self.timestamp),
                 sensor_id=self.sensor_id,
                 latitude=self.latitude,
                 longitude=self.longitude,
--- producer/main.py
+++ producer/main.py
@@ -1,30 +1,30 @@
-from datetime import timedelta
+from datetime import datetime, timedelta
 import asyncio
 import logging as log
 
 from core.runner import Runner
-from core.producers.traffic_producer import TrafficProducer
+from core.producers.temperature_producer import TemperatureProducer
 
 log.basicConfig(level=log.INFO, format='[%(asctime)s] %(levelname)s: %(message)s')
 
 
 def main() -> None:
     runner = Runner(producers=[
-        # TemperatureProducer(
-        #     sensor_id='temperature-sensor-1',
-        #     frequency=timedelta(seconds=1),
-        #     begin_date=datetime(2023, 1, 1, 0, 0, 0),
-        #     latitude=40.416775,
-        #     longitude=-3.703790,
-        #     limit=10,
-        # ),
-        TrafficProducer(
-            sensor_id='traffic-producer-1',
+        TemperatureProducer(
+            sensor_id='temperature-sensor-1',
             frequency=timedelta(seconds=1),
-            limit=10,
+            begin_date=datetime(2023, 1, 1, 0, 0, 0),
             latitude=40.416775,
             longitude=-3.703790,
+            limit=10,
         ),
+        # TrafficProducer(
+        #     sensor_id='traffic-producer-1',
+        #     frequency=timedelta(seconds=1),
+        #     limit=10,
+        #     latitude=40.416775,
+        #     longitude=-3.703790,
+        # ),
     ])
 
     log.debug('Starting runner')
