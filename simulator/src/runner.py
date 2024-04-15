6f2815c5ad7e9d7865a1d8f5447bbaa4452026f5
elenaferr0 <elena.ferro@gmail.com>
2024-04-15 21:01:16 +0200

    feat: loading dynamically avro schemas

--- simulator/src/runner.py
+++ simulator/src/runner.py
@@ -4,26 +4,29 @@ import logging as log
 import signal
 import threading
 import time
+from typing import Dict
 
-import avro.schema
-from avro.io import DatumWriter
+from avro.io import DatumWriter, BinaryEncoder
+from avro.schema import Schema
 from kafka import KafkaProducer
 
 from .models.config.env_config import EnvConfig
-from .serialization.serializer_visitor import SerializerVisitor
 from .simulators.simulator import Simulator
+from .utils.serializer_visitor import SerializerVisitor
 
 
 class Runner:
+    _SUBJECT = 'reading'
 
-    def __init__(self, *, simulators: list[Simulator], config: EnvConfig) -> None:
+    def __init__(self, *, simulators: list[Simulator], config: EnvConfig,
+                 schema_by_subject: Dict[str, tuple[int, Schema]]) -> None:
         self.serializer = SerializerVisitor()
         self.simulators = simulators
         self.topic = config.kafka_topic
         self.max_workers = config.max_workers
+        schema_id, self.avro_schema = schema_by_subject[self._SUBJECT]
 
-        path = './schema_registry/schemas/reading.avsc'
-        schema = avro.schema.parse(open(path).read())
+        self.schema_id_bytes = self._build_avro_bytes_identifier(schema_id)
 
         bootstrap_server = f'{config.kafka_host}:{config.kafka_port}'
         log.debug(f'Connecting to Kafka at {bootstrap_server}')
@@ -34,9 +37,9 @@ class Runner:
                 max_block_ms=config.max_block_ms,
                 acks=1,
             )
-            self._writer = DatumWriter(schema)
+            self._writer = DatumWriter(self.avro_schema)
             self._bytes_writer = io.BytesIO()
-            self._encoder = avro.io.BinaryEncoder(self._bytes_writer)
+            self._encoder = BinaryEncoder(self._bytes_writer)
         except Exception as e:
             log.exception('Error while creating KafkaProducer', e)
 
@@ -49,7 +52,7 @@ class Runner:
             log.debug(f'Stopping {simulator.sensor_name}')
             simulator.stop()
 
-        time.sleep(1.5)  # wait for the simulators to stop
+        time.sleep(2)  # wait for the simulators to stop
         self._producer.close()
         log.debug('Producer closed, exiting.')
 
@@ -65,11 +68,10 @@ class Runner:
                 self._writer.write(json_item, self._encoder)
                 raw_bytes = self._bytes_writer.getvalue()
 
-                schema_id = 1
-                message_with_schema_id = bytearray([0]) + schema_id.to_bytes(4,
-                                                                             'big') + raw_bytes
-
-                self._producer.send(self.topic, message_with_schema_id)
+                self._producer.send(
+                    self.topic,
+                    self._prepend_schema_id(raw_bytes),
+                )
                 log.debug(f'Thread {thread}: sent message to Kafka')
             except Exception as e:
                 log.exception('Error while sending message to Kafka', e)
@@ -78,3 +80,18 @@ class Runner:
         log.debug('Creating thread pool with %d workers', self.max_workers)
         with concurrent.ThreadPoolExecutor(max_workers=self.max_workers) as executor:
             executor.map(self._callback, self.simulators)
+
+    @staticmethod
+    def _build_avro_bytes_identifier(schema_id: int) -> bytes:
+        """
+        Returns the schema id as bytes prepended with the magic byte,
+        which is used by the Avro serializer to identify the schema.
+        """
+        magic_byte = bytearray([0])
+        return magic_byte + schema_id.to_bytes(4, 'big')
+
+    def _prepend_schema_id(self, raw_bytes: bytes) -> bytes:
+        """
+        Concatenates the schema id bytes with the raw bytes of the message.
+        """
+        return self.schema_id_bytes + raw_bytes
