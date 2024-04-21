8f6825db8bf968ac29874542453bde42c687d94c
elenaferr0 <elena.ferro@gmail.com>
2024-04-21 18:38:13 +0200

    chore: fix ruff checks

--- simulator/test/test_string_methods.py
+++ simulator/test/test_string_methods.py
@@ -5,15 +5,15 @@ from simulator.src.models.sensor_type import SensorType
 
 class TestStringMethods(unittest.TestCase):
 
-    def test_sensor_type(self):
-        type = SensorType.TRAFFIC
-        self.assertEqual(type, SensorType.TRAFFIC)
+    def test_sensor_type(self) -> None:
+        t = SensorType.TRAFFIC
+        self.assertEqual(t, SensorType.TRAFFIC)
 
-    def test_isupper(self):
+    def test_isupper(self) -> None:
         self.assertTrue('FOO'.isupper())
         self.assertFalse('Foo'.isupper())
 
-    def test_split(self):
+    def test_split(self) -> None:
         s = 'hello world'
         self.assertEqual(s.split(), ['hello', 'world'])
         # check that s.split fails when the separator is not a string
