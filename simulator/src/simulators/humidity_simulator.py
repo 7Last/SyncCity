6151f764b09700a32b9e9f7d192b197af786c456
Matteo <matteo.tiozzo.1@studenti.unipd.it>
2024-06-15 14:31:13 +0200

    fix humidity sensor

--- simulator/src/simulators/humidity_simulator.py
+++ simulator/src/simulators/humidity_simulator.py
@@ -13,22 +13,14 @@ class HumiditySimulator(Simulator):
 
     def __init__(self, sensor_name: str, config: SensorConfig) -> None:
         super().__init__(sensor_name, config)
-        self._value = _humidity_value(self._timestamp)
-    # def __init__(self, *, sensor_name: str, sensor_uuid: UUID,
-    #              points_spacing: timedelta, latitude: float, longitude: float,
-    #              generation_delay: timedelta = timedelta(seconds=1),
-    #              begin_date: datetime = None,
-    #              limit: int = None) -> None:
-    #     super().__init__(sensor_name=sensor_name, sensor_uuid=sensor_uuid,
-    #                      points_spacing=points_spacing, latitude=latitude,
-    #                      generation_delay=generation_delay, longitude=longitude,
-    #                      begin_date=begin_date, limit=limit)
+        # self._value = _humidity_value(self._timestamp)
+ 
 
     def stream(self) -> Iterable[HumidityRawData]:
         while self._limit != 0 and self._running:
             
             yield HumidityRawData(
-                value=self._value,
+                value= _humidity_value(self._timestamp),
                 sensor_uuid=self._sensor_uuid,
                 sensor_name=self.sensor_name,
                 latitude=self._latitude,
@@ -42,21 +34,25 @@ class HumiditySimulator(Simulator):
             self._timestamp += self._points_spacing
             self._event.wait(self._generation_delay.total_seconds())
 
-
 def _humidity_value(timestamp: datetime) -> float:
     seasonal_coeff = _humidity_seasonal_coefficient(timestamp)
     daily_variation = _humidity_daily_variation(timestamp)
+    night_attenuation = _night_attenuation_factor(timestamp)
 
     hour = timestamp.hour + timestamp.minute / 60
-    noise = random.uniform(-5, 5)
-    return daily_variation * sin(hour * pi / 24) + seasonal_coeff + noise
-
+    noise = random.uniform(-3, 3)  # Raffiniamo la variazione casuale
+    return (daily_variation * sin(hour * pi / 24) + seasonal_coeff + noise) * night_attenuation
 
 def _humidity_seasonal_coefficient(timestamp: datetime) -> float:
     x = timestamp.month + (timestamp.day - 1) / 30
-    return 50 + 10 * sin(x * pi / 12 - 1 / 4)
-
+    return 45 + 15 * sin(x * pi / 12 - 1 / 4)  # Aggiustiamo i coefficienti stagionali
 
 def _humidity_daily_variation(timestamp: datetime) -> float:
     x = timestamp.month + (timestamp.day - 1) / 30
-    return 20 * (sin(x * pi / 12 - (pi - 3) / 2) + 1)
+    return 25 * (sin(x * pi / 12 - (pi - 3) / 2) + 1)  # Modifichiamo la variazione giornaliera
+
+def _night_attenuation_factor(timestamp: datetime) -> float:
+    hour = timestamp.hour
+    if 0 <= hour < 6 or 20 <= hour < 24:
+        return 0.8  # Riduciamo la variazione di umidità durante le ore notturne
+    return 1
