df8c4ca1bca0a968b3dc90625df07a261e66ef35
Matteo <matteo.tiozzo.1@studenti.unipd.it>
2024-06-16 17:53:58 +0200

    prima dashboard livelli fiumi

--- clickhouse/init/8_river_level.sql
+++ clickhouse/init/8_river_level.sql
@@ -32,7 +32,7 @@ CREATE TABLE sensors.river_level_daily
 (
     sensor_name         String,
     date                DateTime64,
-    avg_humidity        Float32,
+    avg_river_level       Float32,
     insertion_timestamp DateTime64(6) default now64()
 ) ENGINE = MergeTree()
       ORDER BY (sensor_name, date);
@@ -42,25 +42,6 @@ CREATE MATERIALIZED VIEW sensors.river_level_daily_mv
 SELECT sensor_name,
        toStartOfDay(timestamp) AS date,
        avg(value)               AS avg_river_level
-FROM sensors.river_level
-GROUP BY sensor_name, date;
-
-
--- Yearly river_level
-CREATE TABLE sensors.river_level_yearly
-(
-    sensor_name         String,
-    date                Date,
-    avg_river_level        Float32,
-    insertion_timestamp DateTime64(6) default now64()
-) ENGINE = MergeTree()
-      ORDER BY (sensor_name, date);
-
-CREATE MATERIALIZED VIEW sensors.river_level_yearly_mv
-    TO sensors.river_level_yearly AS
-SELECT sensor_name,
-       toStartOfWeek(timestamp) AS date,
-       avg(value)              AS avg_river_level
 FROM sensors.river_level
 GROUP BY sensor_name, date;
 
@@ -82,3 +63,20 @@ SELECT sensor_name,
 FROM sensors.river_level
 GROUP BY sensor_name, date;
 
+-- Yearly river_level
+CREATE TABLE sensors.river_level_yearly
+(
+    sensor_name         String,
+    date                Date,
+    avg_river_level        Float32,
+    insertion_timestamp DateTime64(6) default now64()
+) ENGINE = MergeTree()
+      ORDER BY (sensor_name, date);
+
+CREATE MATERIALIZED VIEW sensors.river_level_yearly_mv
+    TO sensors.river_level_yearly AS
+SELECT sensor_name,
+       toStartOfWeek(timestamp) AS date,
+       avg(value)              AS avg_river_level
+FROM sensors.river_level
+GROUP BY sensor_name, date;
