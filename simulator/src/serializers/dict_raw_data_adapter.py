dfeca75ddb557ffa6679e2fadf3f754ecc5395d3
elenaferr0 <elena.ferro@gmail.com>
2024-07-11 15:28:50 +0200

    fix: converting dict to bytes in json_serialization_strategy.py

--- simulator/src/serializers/dict_raw_data_adapter.py
+++ simulator/src/serializers/dict_raw_data_adapter.py
@@ -8,21 +8,19 @@ from simulator.src.models.raw_data.raw_data import RawData
 class DictRawDataAdapter(dict):
     def __init__(self, raw_data: RawData):
         self.__raw_data = raw_data
-        classname = self.__raw_data.__class__.__name__
         items = self.__raw_data.__dict__.items()
         super().__init__({
-            self.__beautify_key(k, classname): self.__beautify_value(v)
+            self.__beautify_key(k): self.__beautify_value(v)
             for k, v in items
         })
 
-    @staticmethod
-    def __beautify_key(key: str, classname: str) -> str:
+    def __beautify_key(self, key: str) -> str:
+        classname = self.__raw_data.__class__.__name__
         key = re.sub("^_+", "", key)
         key = re.sub(f"{classname}_+", "", key)
         return key
 
-    @staticmethod
-    def __beautify_value(value) -> any:
+    def __beautify_value(self, value) -> any:
         if isinstance(value, UUID):
             return str(value)
         if isinstance(value, datetime):
--- simulator/src/serializers/json_serialization_strategy.py
+++ simulator/src/serializers/json_serialization_strategy.py
@@ -1,8 +1,10 @@
+import json
+
 from simulator.src.models.raw_data.raw_data import RawData
 from simulator.src.serializers.dict_raw_data_adapter import DictRawDataAdapter
 from simulator.src.serializers.serialization_strategy import SerializationStrategy
 
 
 class JsonSerializationStrategy(SerializationStrategy):
-    def serialize(self, raw_data: RawData) -> dict:
-        return DictRawDataAdapter(raw_data)
+    def serialize(self, raw_data: RawData) -> bytes:
+        return json.dumps(DictRawDataAdapter(raw_data)).encode('utf-8')
